71f68d08a54d766ffd0b55896811be4aeec455fd
ariannejr <arianne.inoc@usjr.edu.ph>
2022-10-29 18:41:29 +0800

    Layed out inventory for main menu

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
-    
+    public GameObject Inventory;
+    public GameObject PowerUpsInventory;
+    public GameObject GiftsInventory;
     public void OpenPanel()
     {
         if(Panel != null){
             bool isActive = Panel.activeSelf;
             Panel.SetActive(!isActive);
         }
+    }
+
+    public void GotoInventory()
+    {
+        Inventory.SetActive(true);
+    }
+
+    public void CloseInventory()
+    {
+        Inventory.SetActive(false);
+    }
+
+    public void OpenGiftsInventory()
+    {
+        GiftsInventory.SetActive(true);
+        PowerUpsInventory.SetActive(false);
+    }
+
+    public void OpenPowerUpsInventory()
+    {
+        PowerUpsInventory.SetActive(true);
+        GiftsInventory.SetActive(false);
     }
 }
