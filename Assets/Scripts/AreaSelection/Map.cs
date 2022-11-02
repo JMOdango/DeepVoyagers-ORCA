73d736a6e87b09640ab885175a7663f63080f971
JM Oda <koffeetan07@gmail.com>
2022-11-03 00:20:48 +0800

    changes

---
+++ Assets/Scripts/AreaSelection/Map.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Map", menuName = "Scriptable Objects/Maps")]
+
+public class Map : ScriptableObject
+{
+    public int mapIndex;
+    public Sprite mapImage;
+    public Object sceneToLoad; 
+    // public bool isUnlock = false;
+    // public GameObject lockGo;
+    // public GameObject unlockGo;
+
+    // private void Update()
+    // {
+    //     UpdateMapStatus();
+    // }
+
+    // private void UpdateMapStatus()
+    // {
+    //     if(isUnlock)
+    //     {
+    //         unlockGo.gameObject.SetActive(true);
+    //         lockGo.gameObject.SetActive(false);
+    //     }
+    //     else
+    //     {
+    //         unlockGo.gameObject.SetActive(false);
+    //         lockGo.gameObject.SetActive(true);
+    //     }
+    // }
+    
+}
