2f0b88dc58b04e1ba333701c9a5a33617177c84e
JM Oda <koffeetan07@gmail.com>
2023-01-26 22:05:33 +0800

    changes

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -3,21 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class InventoryUI : MonoBehaviour
 {   
     public static InventoryUI inventoryUI;
     InventoryManager inventory;
     [Header("Powerups-Text")]
-    public TextMeshProUGUI smallenergytext, mediumenergytext, largeenergytext, mysterysnacktext, magnettext, neptunestridenttext, voidgemtext, 
-    nettext, fungitext, pocketwatchtext, mermaidsorbtext, baskettext;
+    public TextMeshProUGUI smallenergytext;
+    public TextMeshProUGUI mediumenergytext; 
+    public TextMeshProUGUI largeenergytext;
+    public TextMeshProUGUI mysterysnacktext; 
+    public TextMeshProUGUI magnettext;
+    public TextMeshProUGUI neptunestridenttext; 
+    public TextMeshProUGUI voidgemtext; 
+    public TextMeshProUGUI nettext;
+    public TextMeshProUGUI fungitext;
+    public TextMeshProUGUI pocketwatchtext; 
+    public TextMeshProUGUI mermaidsorbtext;
+    public TextMeshProUGUI baskettext;
+
     [Header("Gifts-Text")]
-    public TextMeshProUGUI stufftoytext, waterproofcameratext, maptext, historybooktext, seaweedtext, crystalstext, toyfiguretext;
+    public TextMeshProUGUI stufftoytext;
+    public TextMeshProUGUI waterproofcameratext;
+    public TextMeshProUGUI maptext;
+    public TextMeshProUGUI historybooktext;
+    public TextMeshProUGUI seaweedtext, crystalstext;
+    public TextMeshProUGUI toyfiguretext;
+
     [Header("Powerups-Button")]
-    public Button smallenergybtn, mediumenergybtn, largeenergybtn, mysterysnackbtn, magnetbtn, neptunestridentbtn, voidgembtn, 
-    netbtn, fungibtn, pocketwatchbtn, mermaidsorbbtn, basketbtn;
+    public Button smallenergybtn;
+    public Button mediumenergybtn;
+    public Button largeenergybtn;
+    public Button mysterysnackbtn;
+    public Button magnetbtn;
+    public Button neptunestridentbtn;
+    public Button voidgembtn;
+    public Button netbtn;
+    public Button fungibtn;
+    public Button pocketwatchbtn;
+    public Button mermaidsorbbtn;
+    public Button basketbtn;
+
     [Header("Gifts-Button")]
-    public TextMeshProUGUI stufftoybtn, waterproofcamerabtn, mapbtn, historybookbtn, seaweedbtn, crystalsbtn, toyfigurebtn;
+    public Button stufftoybtn;
+    public Button waterproofcamerabtn;
+    public Button  mapbtn;
+    public Button historybookbtn;
+    public Button seaweedbtn;
+    public Button crystalsbtn;
+    public Button toyfigurebtn;
 
     public void Awake(){
         if(inventoryUI == null)
@@ -45,6 +80,8 @@ public class InventoryUI : MonoBehaviour
         int mermaidsorbcount, 
         int basketcount
     ){
+        Scene scene = SceneManager.GetActiveScene();
+
         smallenergytext.text = smallenergycount.ToString();
         mediumenergytext.text = mediumenergycount.ToString();
         largeenergytext.text = largeenergycount.ToString();
@@ -58,14 +95,129 @@ public class InventoryUI : MonoBehaviour
         mermaidsorbtext.text = mermaidsorbcount.ToString();
         baskettext.text = basketcount.ToString();
 
-        if(smallenergycount != 0)
+        if(scene.name == "MainMenu")
         {
-            smallenergybtn.interactable = true;
+            if(smallenergycount != 0)
+            {
+                smallenergybtn.interactable = true;
+            }
+            else
+            {
+                smallenergybtn.interactable = false;
+            }
+
+            if(mediumenergycount != 0)
+            {
+                mediumenergybtn.interactable = true;
+            }
+            else
+            {
+                mediumenergybtn.interactable = false;
+            }
+
+            if(largeenergycount != 0)
+            {
+                largeenergybtn.interactable = true;
+            }
+            else
+            {
+                largeenergybtn.interactable = false;
+            }
+            
+            mysterysnackbtn.interactable = false;
+            magnetbtn.interactable = false;
+            neptunestridentbtn.interactable = false;
+            voidgembtn.interactable = false;
+            netbtn.interactable = false;
+            fungibtn.interactable = false;
+            pocketwatchbtn.interactable = false;
+            mermaidsorbbtn.interactable = false;
+            basketbtn.interactable = false;
         }
         else
         {
-            smallenergybtn.interactable = false;
+            if(mysterysnackcount != 0)
+            {
+                mysterysnackbtn.interactable = true;
+            }
+            else
+            {
+                mysterysnackbtn.interactable = false;
+            }
+
+            if(magnetcount != 0)
+            {
+                magnetbtn.interactable = true;
+            }
+            else
+            {
+                magnetbtn.interactable = false;
+            }
+
+            if(neptunestridentcount != 0)
+            {
+                neptunestridentbtn.interactable = true;
+            }
+            else
+            {
+                neptunestridentbtn.interactable = false;
+            }
+
+            if(voidgemcount != 0)
+            {
+                voidgembtn.interactable = true;
+            }
+            else
+            {
+                voidgembtn.interactable = false;
+            }
+
+            if(netcount != 0)
+            {
+                netbtn.interactable = true;
+            }
+            else
+            {
+                netbtn.interactable = false;
+            }
+
+            if(fungicount != 0)
+            {
+                fungibtn.interactable = true;
+            }
+            else
+            {
+                fungibtn.interactable = false;
+            }
+
+            if(pocketwatchcount != 0)
+            {
+                pocketwatchbtn.interactable = true;
+            }
+            else
+            {
+                pocketwatchbtn.interactable = false;
+            }
+
+            if(mermaidsorbcount != 0)
+            {
+                mermaidsorbbtn.interactable = true;
+            }
+            else
+            {
+                mermaidsorbbtn.interactable = false;
+            }
+
+            if(basketcount != 0)
+            {
+                basketbtn.interactable = true;
+            }
+            else
+            {
+                basketbtn.interactable = false;
+            }
         }
+
     }
 
        public void SetUIGifts(int stufftoycount, 
@@ -83,7 +235,68 @@ public class InventoryUI : MonoBehaviour
         seaweedtext.text = seaweedcount.ToString();
         crystalstext.text = crystalscount.ToString();
         toyfiguretext.text = toyfigurecount.ToString();
+
+        if(stufftoycount != 0)
+        {
+            stufftoybtn.interactable = true;
+        }
+        else
+        {
+            stufftoybtn.interactable = false;
+        }
+
+        if(waterproofcameracount != 0)
+        {
+            waterproofcamerabtn.interactable = true;
+        }
+        else
+        {
+            waterproofcamerabtn.interactable = false;
+        }
+
+        if(mapcount != 0)
+        {
+            mapbtn.interactable = true;
+        }
+        else
+        {
+            mapbtn.interactable = false;
+        }
+
+        if(historybookcount != 0)
+        {
+            historybookbtn.interactable = true;
+        }
+        else
+        {
+            historybookbtn.interactable = false;
+        }
+
+        if(seaweedcount != 0)
+        {
+            seaweedbtn.interactable = true;
+        }
+        else
+        {
+            seaweedbtn.interactable = false;
+        }
+
+        if(crystalscount != 0)
+        {
+            crystalsbtn.interactable = true;
+        }
+        else
+        {
+            crystalsbtn.interactable = false;
+        }
+
+        if(toyfigurecount != 0)
+        {
+            toyfigurebtn.interactable = true;
+        }
+        else
+        {
+            toyfigurebtn.interactable = false;
+        }
     }
-
-
 }
